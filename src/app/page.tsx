9e5d4f42ea01d11842b3d5124f74a1bec0469a65
edgars <edzhuks@gmail.com>
2024-09-05 21:17:44 +0300

    search params for search and sort

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,23 @@ import { useSuspenseQuery } from '@apollo/client'
 import { allCharacters, characterShort } from '../queries'
 import CharacterCard from '../components/CharacterCard'
 import { useState } from 'react'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 
 export default function PollPage() {
   const { data }: { data: { allPeople: { people: [characterShort] } } } =
     useSuspenseQuery(allCharacters)
 
-  const [searchQuery, setSearchQuery] = useState('')
-  const [sortAscending, setSortAscending] = useState(true)
+  const searchParams = useSearchParams()
+
+  const [searchQuery, setSearchQuery] = useState(
+    searchParams.get('search') || ''
+  )
+  const [sortAscending, setSortAscending] = useState(
+    searchParams.get('sortOrder') !== 'desc'
+  )
+
+  const pathname = usePathname()
+  const router = useRouter()
 
   return (
     <div>
@@ -21,14 +31,30 @@ export default function PollPage() {
           className="w-full rounded-xl p-4 uppercase shadow-lg font-semibold text-xl placeholder:font-normal placeholder:text-base placeholder:normal-case focus:outline-none  focus:ring-slate-400 focus:ring-2"
           placeholder="Search..."
           value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
+          onChange={(e) => {
+            setSearchQuery(e.target.value)
+            router.replace(
+              `${pathname}?search=${e.target.value}&sortOrder=${sortAscending}`,
+              {
+                scroll: false,
+              }
+            )
+          }}
         />
       </div>
       <div className="flex px-3 py-1 text-lg font-semibold text-slate-500">
         <div className="flex-1 ">Sort</div>
         <button
           className="cursor-pointer"
-          onClick={() => setSortAscending(!sortAscending)}>
+          onClick={() => {
+            setSortAscending(!sortAscending)
+            router.replace(
+              `${pathname}?search=${searchQuery}&sortOrder=${!sortAscending}`,
+              {
+                scroll: false,
+              }
+            )
+          }}>
           Name {sortAscending ? '↓' : '↑'}
         </button>
       </div>
